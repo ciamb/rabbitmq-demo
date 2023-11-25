cd85b2413151701766ebc9fab986239a23c3dc28
ciamb <andreaciambella@outlook.it>
2023-11-25 21:22:45 +0100

    added message durability variable

--- src/main/java/com/ciamb/rubbitmqdemo/taskdemo/NewTask.java
+++ src/main/java/com/ciamb/rubbitmqdemo/taskdemo/NewTask.java
@@ -3,9 +3,12 @@ package com.ciamb.rubbitmqdemo.taskdemo;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class NewTask {
     private final static String QUEUE_NAME = "Hello";
+    private final static String TASK_QUEUE = "task_queue"
+
 
     public static void main(String[] args) {
         ConnectionFactory factory = new ConnectionFactory();
@@ -14,9 +17,16 @@ public class NewTask {
 
         try (Connection connection = factory.newConnection()) {
             Channel channel = connection.createChannel();
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            boolean durable = true;
+            channel.queueDeclare(TASK_QUEUE, durable, false, false, null);
+
             var message = String.join(" ", argv);
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+            channel.basicPublish("",
+                    TASK_QUEUE,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes());
+
             System.out.println(" [x] sent '" + message + "'");
         } catch (Exception e) {
             System.out.println("unhandled exception: " + e);
