742e362bd64775ae0a081d448706aedc8e4140af
Andrea Ciambella <andrea.ciambella@unikey.it>
2023-12-06 14:49:36 +0100

    added demo for multiple subscription in exchange

---
+++ src/main/java/com/ciamb/rubbitmqdemo/subscribedemo/ReceiveLogs.java
@@ -0,0 +1,49 @@
+package com.ciamb.rubbitmqdemo.subscribedemo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class ReceiveLogs {
+
+    private final static String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME,"fanout");
+        var queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+R");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [*] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } catch (Exception e) {
+                System.out.println("unhandled error: " + e);
+            } finally {
+                System.out.println(" [*] done");
+            }
+        };
+        boolean autoAck = true;
+        channel.basicConsume(queueName, autoAck, deliverCallback, consumerTag -> { });
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+}
